196e7189a5fbd7e7190d6d79bfd8909d617ea8bb
kurdtcode <kevinddmm@gmail.com>
2024-05-22 10:33:38 +0700

    te

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,22 +2,29 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Partitur;
 
-class DatabaseSeeder extends Seeder
+class PartiturSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
+     *
+     * @return void
      */
-    public function run(): void
+    public function run()
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        Partitur::create([
+            'judul' => 'Ave Maria',
+            'pembuat_aransemen' => 'Franz Biebl',
+            'komposer' => 'Franz Biebl',
+            'genre_lagu' => 'Religi',
+            'link_youtube' => 'https://www.youtube.com/watch?v=0Kvbj_i9D6A',
+            'kebutuhan_instrumen' => 'Piano',
+            'tingkat_kesulitan' => 'Sulit',
+            'kebutuhan_solo' => true,
+            'jenis_suara' => 'SATB'
         ]);
+
     }
 }
